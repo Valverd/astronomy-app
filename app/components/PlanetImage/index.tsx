5ec09fb3e806b2009ba04ea4124883894986ddef
renan.valverde <renanvgmartins@hotmail.com>
2025-05-31 09:53:40 -0300

    Erro ao carregar imagens da barra de planetas

--- app/components/PlanetImage/index.tsx
+++ app/components/PlanetImage/index.tsx
@@ -10,8 +10,8 @@ export default function PlanetImage({ planet, updatePlanet }: PlanetImageProps) 
 
     return (
         <Image
-            src={`/${planet.name}.png`}
-            alt={planet.name}
+            src={`/${planet.id}.png`}
+            alt={planet.id}
             width={50}
             height={50}
             className="hover:scale-[1.1] duration-300 cursor-pointer drop-shadow-[6px_5px_8px_rgba(0,0,0,0.4)]"
